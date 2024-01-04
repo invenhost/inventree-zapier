4ebbe0cd4751da17c1609688bf71ec0018d48a0c
Matthias Mair <code@mjmair.com>
2024-01-04 17:42:08 +0100

    style fixes

--- src/inventree_zapier/ZapierPlugin.py
+++ src/inventree_zapier/ZapierPlugin.py
@@ -1,6 +1,4 @@
-"""
-Plugin to integrate Zapier into InvenTree.
-"""
+"""Plugin to integrate Zapier into InvenTree."""
 
 import json
 
@@ -10,10 +8,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 # InvenTree plugin libs
 from plugin import InvenTreePlugin
-from plugin.mixins import UrlsMixin, APICallMixin, AppMixin, EventMixin
+from plugin.mixins import APICallMixin, AppMixin, EventMixin, UrlsMixin
 
 
 class ZapierPlugin(AppMixin, APICallMixin, EventMixin, UrlsMixin, InvenTreePlugin):
+    """Integrate Zapier into InvenTree."""
 
     AUTHOR = "Matthias Mair"
     DESCRIPTION = "Zapier plugin for InvenTree"
@@ -23,44 +22,62 @@ class ZapierPlugin(AppMixin, APICallMixin, EventMixin, UrlsMixin, InvenTreePlugi
     TITLE = "Zapier connector for InvenTree"
 
     def view_connection_test(self, request):
-        """for testing the connection"""
-        return JsonResponse({'user': request.user.username}, safe=False)
+        """For testing the connection."""
+        return JsonResponse({"user": request.user.username}, safe=False)
 
     @csrf_exempt
     def view_event_reg(self, request):
-        """register a hook"""
+        """Register a hook."""
         from .models import ZapierHook
 
         ZapierHook.objects.create(hookurl=self.get_hookurl(request))
-        return JsonResponse({'response': 'ok'})
+        return JsonResponse({"response": "ok"})
 
     def get_hookurl(self, request):
-        ret = json.loads(str(request.body, 'utf-8'))
-        return ret.get('hookUrl')
+        """Get the hookurl from the request."""
+        ret = json.loads(str(request.body, "utf-8"))
+        return ret.get("hookUrl")
 
     @csrf_exempt
     def view_event_unsub(self, request):
-        """unregister a hook"""
+        """Unregister a hook."""
         from .models import ZapierHook
 
         obj = ZapierHook.objects.filter(hookurl=self.get_hookurl(request))
         if obj:
             obj.delete()
-            return JsonResponse({'response': 'ok. Hook deleted'})
-        return JsonResponse({'response': 'Hook not found'})
+            return JsonResponse({"response": "ok. Hook deleted"})
+        return JsonResponse({"response": "Hook not found"})
 
     def view_event_list(self, request):
-        """for getting a sample list"""
-        return JsonResponse([
-            {'event': 'instance.created', 'id': 1, 'model': 'Part', 'table': 'part.Part', 'args': '', 'kwargs': ''},
-            {'event': 'instance.saved', 'id': 1, 'model': 'Part', 'table': 'part.Part', 'args': '', 'kwargs': ''}
-        ], safe=False)
+        """For getting a sample list."""
+        return JsonResponse(
+            [
+                {
+                    "event": "instance.created",
+                    "id": 1,
+                    "model": "Part",
+                    "table": "part.Part",
+                    "args": "",
+                    "kwargs": "",
+                },
+                {
+                    "event": "instance.saved",
+                    "id": 1,
+                    "model": "Part",
+                    "table": "part.Part",
+                    "args": "",
+                    "kwargs": "",
+                },
+            ],
+            safe=False,
+        )
 
     def process_event(self, event, *args, **kwargs):
-        """ Custom event processing """
+        """Custom event processing."""
         from .models import ZapierHook
 
-        if kwargs.get('model') == 'ZapierHook':
+        if kwargs.get("model") == "ZapierHook":
             return
 
         obj = ZapierHook.objects.all()
@@ -70,22 +87,23 @@ class ZapierPlugin(AppMixin, APICallMixin, EventMixin, UrlsMixin, InvenTreePlugi
                     item.hookurl,
                     endpoint_is_url=True,
                     simple_response=False,
-                    method='POST',
+                    method="POST",
                     data={
-                        'event': event,
-                        'model': str(kwargs.get('model', '')),
-                        'table': str(kwargs.get('table', '')),
-                        'id': str(kwargs.get('id', '')),
-                        'args': str(args),
-                        'kwargs': str(kwargs)
-                    }
+                        "event": event,
+                        "model": str(kwargs.get("model", "")),
+                        "table": str(kwargs.get("table", "")),
+                        "id": str(kwargs.get("id", "")),
+                        "args": str(args),
+                        "kwargs": str(kwargs),
+                    },
                 )
                 print(ret)
 
     def setup_urls(self):
+        """URLs for app."""
         return [
-            path(r'^test/', self.view_connection_test, name='test'),
-            path(r'^event/register/', self.view_event_reg, name='event-register'),
-            path(r'^event/unsub/', self.view_event_unsub, name='event-unsub'),
-            path(r'^event/list/', self.view_event_list, name='event-list'),
+            path(r"^test/", self.view_connection_test, name="test"),
+            path(r"^event/register/", self.view_event_reg, name="event-register"),
+            path(r"^event/unsub/", self.view_event_unsub, name="event-unsub"),
+            path(r"^event/list/", self.view_event_list, name="event-list"),
         ]
--- src/inventree_zapier/models.py
+++ src/inventree_zapier/models.py
@@ -1,14 +1,29 @@
+"""Models for ZapierPlugin."""
 from django.db import models
 
 
 class EventRegistration(models.Model):
-    """Keeps a list of all registered events"""
+    """Keeps a list of all registered events."""
 
-    event = models.CharField(max_length=250, )
-    model = models.CharField(max_length=250, )
+    event = models.CharField(
+        max_length=250,
+    )
+    model = models.CharField(
+        max_length=250,
+    )
+
+    def __str__(self):
+        """Get string representation of event."""
+        return str(self.event)
 
 
 class ZapierHook(models.Model):
-    """Keeps a list of all registered zaier callback hooks"""
+    """Keeps a list of all registered zaier callback hooks."""
 
-    hookurl = models.CharField(max_length=250, )
+    hookurl = models.CharField(
+        max_length=250,
+    )
+
+    def __str__(self):
+        """Get string representation of hook."""
+        return str(self.hookurl)
